d670c324e7ef61b1cdf63ddd6ff5277b7d309f4b
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-29 14:24:09 -0400

    categorizeByListId started; smarter to sort alphabetically first

--- src/components/HiringDataList.tsx
+++ src/components/HiringDataList.tsx
@@ -1,12 +1,25 @@
 import '../styles/HiringDataList.css';
 import { HiringData } from './Main';
+import { useState } from 'react';
 
 interface HiringDataListProps {
   hiringData: HiringData[] | undefined;
 }
 
+const categorizeByListId = (hiringData: HiringData[]) => {
+  const categorizedHiringData = {}
+  hiringData.forEach(item => {
+    console.log(item)  
+  })
+
+  return categorizedHiringData
+};
+
 const HiringDataList: React.FC<HiringDataListProps> = ({ hiringData }) => {
-  console.log('hiring data',hiringData)
+  const [listIdData, setListIdData] = useState<any>();
+  if(hiringData){
+    categorizeByListId(hiringData)
+  }
   return <div>Hiring Data List</div>;
 };
 
