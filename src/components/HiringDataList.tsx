8c0fe23e75444240ea716838e405a32e2a47b15d
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-29 18:36:10 -0400

    added comments and did some light refactoring

--- src/components/HiringDataList.tsx
+++ src/components/HiringDataList.tsx
@@ -11,12 +11,15 @@ export interface HiringDataListProps {
   hiringData: HiringData[] | undefined;
 }
 
+//Takes in an array of objects and sorts them into bins based on the listId.
+// Dynamic, so that more listId's can be added in the future without affecting
+// funciontality.
 const categorizeByListId = (hiringData: HiringData[]) => {
   const categorizedHiringData: categorizedHiringData = {};
   hiringData.forEach((item) => {
     const { listId } = item;
     if (categorizedHiringData[listId]) {
-      categorizedHiringData[listId] = [...categorizedHiringData[listId], item];
+      categorizedHiringData[listId].push(item);
     } else {
       categorizedHiringData[listId] = [item];
     }
@@ -24,6 +27,10 @@ const categorizeByListId = (hiringData: HiringData[]) => {
   return categorizedHiringData;
 };
 
+//Takes in and array of objects that has been filtered to only include instances
+// that have a name value, and returns an array that has been sorted by name
+// value. The substring method removes excess 'Item- ' from the string so it can
+// be parsed.
 const alphabetizeHiringData = (hiringData: HiringData[]) => {
   const alphabetizedHiringData = hiringData.sort((a, b): number => {
     const aName = parseInt(a.name.substring(5, a.name.length));
@@ -40,20 +47,26 @@ const HiringDataList: React.FC<HiringDataListProps> = ({ hiringData }) => {
 
   useEffect(() => {
     if (hiringData) {
+      //Alphabetize all data first, because it will be in order as it is pushed
+      // into its listId bin by categorizeByListId
       const alphabetizedData = alphabetizeHiringData(hiringData);
       const categorizedHiringData = categorizeByListId(alphabetizedData);
       setListIdData(categorizedHiringData);
     }
   }, [hiringData]);
 
+  //Check if listIdData is truthy (contains objects) so that .values() doesn't
+  // throw an error looking for values of 'undefined'. Returns an array of 
+  // HiringDataCategory components sorted in order of listId.
   const hiringDataListDisplay =
     listIdData &&
     Object.values<HiringData[]>(listIdData).map((listIdCategory) => {
+      const listId = listIdCategory[0].listId;
       return (
         <HiringDataCategory
-          key={`listId-${listIdCategory[0].listId}`}
+          key={`listId-${listId}`}
           categoryData={listIdCategory}
-          name={`List Id - ${listIdCategory[0].listId}`}
+          name={`List Id - ${listId}`}
         />
       );
     });
