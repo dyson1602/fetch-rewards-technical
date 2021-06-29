a5d958b7475cbd39bb85f22838bc5ecf8ddae73a
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-29 14:35:23 -0400

    alphabetizeHiringData function implemented

--- src/components/HiringDataList.tsx
+++ src/components/HiringDataList.tsx
@@ -7,18 +7,29 @@ interface HiringDataListProps {
 }
 
 const categorizeByListId = (hiringData: HiringData[]) => {
-  const categorizedHiringData = {}
-  hiringData.forEach(item => {
-    console.log(item)  
-  })
+  const categorizedHiringData = {};
+  hiringData.forEach((item) => {
+    // console.log(item);
+  });
+  return categorizedHiringData;
+};
 
-  return categorizedHiringData
+const alphabetizeHiringData = (hiringData: HiringData[]) => {
+  const alphabetizedHiringData = hiringData.sort((a, b): number => {
+    const aName = parseInt(a.name.substring(5, a.name.length));
+    const bName = parseInt(b.name.substring(5, b.name.length));
+    if (aName > bName) return 1;
+    if (aName < bName) return -1;
+    return 0;
+  });
+  return alphabetizedHiringData;
 };
 
 const HiringDataList: React.FC<HiringDataListProps> = ({ hiringData }) => {
   const [listIdData, setListIdData] = useState<any>();
-  if(hiringData){
-    categorizeByListId(hiringData)
+  if (hiringData) {
+    alphabetizeHiringData(hiringData);
+    // categorizeByListId(hiringData);
   }
   return <div>Hiring Data List</div>;
 };
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -24,7 +24,6 @@ const Main: React.FC = () => {
     setHiringData(filteredHiringData);
   };
 
-  console.log(hiringData)
   return (
     <div>
       <h2>Main</h2>
