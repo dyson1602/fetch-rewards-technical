662cf91d22c160578298983b2f7ebf77d90d6c54
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-29 13:55:27 -0400

    test button added to fetch data from API; HiringData interface added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,27 @@
-import '../styles/Main.css'
+import '../styles/Main.css';
+import { useState } from 'react';
+
+interface HiringData {
+  id: number;
+  listId: number;
+  name: string;
+}
 
 export const Main: React.FC = () => {
-  return(
+  const [hiringData, setHiringData] = useState<HiringData[]>();
+
+  const fetchDataHandler = async () => {
+    const response = await fetch(
+      'https://fetch-hiring.s3.amazonaws.com/hiring.json'
+    );
+    const data: HiringData[] = await response.json();
+    setHiringData(data);
+  };
+
+  return (
     <div>
-      MAIN
+      <h2>Main</h2>
+      <button onClick={fetchDataHandler}>Fetch data</button>
     </div>
-  )
-}
+  );
+};
